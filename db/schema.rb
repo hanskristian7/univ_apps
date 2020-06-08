2ef466c21f0ccba75d817f84500e10028164d7d6
hanskristian7 <hans.kristian1509@gmail.com>
2020-06-08 19:16:50 +0700

    add flash messages and sign up students

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200608100014) do
+ActiveRecord::Schema.define(version: 20200608112302) do
 
   create_table "courses", force: :cascade do |t|
     t.string "short_name"
@@ -18,6 +18,14 @@ ActiveRecord::Schema.define(version: 20200608100014) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "students", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
